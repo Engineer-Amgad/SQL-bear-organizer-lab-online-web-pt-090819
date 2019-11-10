8b82ac0dc1ec369abb3b4c511d21409e48fa5360
Amgad Malik <amgadmalik@yahoo.com>
2019-11-10 13:38:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,4 +3,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy'
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'Female', 'Brown', 'Mad', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, "Male", 'Brown', 'cool', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, "Female", 'Black', 'quite', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1,'Male' ,'Brown' ,'nice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 1, 'Male', 'Brown', 'nice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'Female', 'black', ''
